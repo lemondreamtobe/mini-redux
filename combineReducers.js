f904d4c24e72ee942930f9678140d2da1ca46a14
USER-20170808KK\Administrator <897418436@qq.com>
2020-11-11 19:28:55 +0800

    del

---
+++ combineReducers.js
@@ -0,0 +1,31 @@
+export default function combineReducers(reducers) {
+    const reducerKeys = Object.keys(reducers)
+    const finalReducers = {}
+
+    // 一一拷贝
+    for (let i = 0; i < reducerKeys.length; i++) {
+        const key = reducerKeys[i]
+
+        if (typeof reducers[key] === 'function') {
+            finalReducers[key] = reducers[key]
+        }
+    }
+    const finalReducerKeys = Object.keys(finalReducers)
+
+    // 这里是关键，返回一个Reducer
+    return function combination(state = {}, action) {
+        let hasChanged = false
+        const nextState = {}
+
+        for (let i = 0; i < finalReducerKeys.length; i++) {
+            const key = finalReducerKeys[i]
+            const reducer = finalReducers[key]
+            const previousStateForKey = state[key]
+            const nextStateForKey = reducer(previousStateForKey, action)
+            nextState[key] = nextStateForKey
+            hasChanged = hasChanged || nextStateForKey !== previousStateForKey
+        }
+        hasChanged = hasChanged || finalReducerKeys.length !== Object.keys(state).length
+        return hasChanged ? nextState : state
+    }
+}
